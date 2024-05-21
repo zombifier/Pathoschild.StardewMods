7f9bb143ae943a02542df6635356e8ac26abb758
weizinai <1776061683@qq.com>
2024-05-21 21:57:53 +0800

    Modified the method of solving the freezing time based on the Horse Flute Handler

--- FastAnimations/Handlers/ReadBookHandler.cs
+++ FastAnimations/Handlers/ReadBookHandler.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Pathoschild.Stardew.FastAnimations.Framework;
 using StardewValley;
 using SObject = StardewValley.Object;
@@ -9,12 +9,6 @@ namespace Pathoschild.Stardew.FastAnimations.Handlers;
 /// <remarks>See game logic in <see cref="SObject.readBook"/>.</remarks>
 internal class ReadBookHandler : BaseAnimationHandler
 {
-    /*********
-     ** Field
-     *********/
-    /// <summary>Whether the read-book animation is End.</summary>
-    private bool IsReadingBookEnd;
-
     /*********
      ** Public methods
      *********/
@@ -28,46 +22,17 @@ internal class ReadBookHandler : BaseAnimationHandler
     /// <param name="playerAnimationID">The player's current animation ID.</param>
     public override bool IsEnabled(int playerAnimationID)
     {
-        var player = Game1.player;
-
-        // Handler remaining pause time after the end of the animation
-        if (this.IsReadingBookEnd)
-        {
-            this.IsReadingBookEnd = false;
-            player.forceCanMove();
-        }
-
-        return this.IsReadingBook(player);
+        return Game1.player.FarmerSprite.currentAnimation is [{ frame: 57, milliseconds: 1000 }, ..];
     }
 
     /// <summary>Perform any logic needed on update while the animation is active.</summary>
     /// <param name="playerAnimationID">The player's current animation ID.</param>
     public override void Update(int playerAnimationID)
     {
-        var player = Game1.player;
-        var location = player.currentLocation;
+        this.SpeedUpPlayer(() => !this.IsEnabled(playerAnimationID));
 
-        this.ApplySkips(
-            () =>
-            {
-                // player animation
-                player.Update(Game1.currentGameTime, location);
-
-                this.IsReadingBookEnd = !this.IsReadingBook(player);
-            },
-            () => !this.IsReadingBook(player)
-        );
-    }
-
-    /*********
-     ** Private methods
-     *********/
-    /// <summary>Check whether the current player's animation is the animation of reading book.</summary>
-    /// <remarks>Derived from <see cref="SObject.readBook"/>.</remarks>
-    private bool IsReadingBook(Farmer player)
-    {
-        List<FarmerSprite.AnimationFrame>? currentAnimation = player.FarmerSprite.CurrentAnimation;
-
-        return currentAnimation is [{ frame: 57, milliseconds: 1000 }, ..];
+        // reduce freeze time
+        int reduceTimersBy = (int)(BaseAnimationHandler.MillisecondsPerFrame * this.Multiplier);
+        Game1.player.freezePause = Math.Max(0, Game1.player.freezePause - reduceTimersBy);
     }
 }
