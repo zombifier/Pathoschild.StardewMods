f752767a6003340cfa95e4cfbf97d6309f540df1
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2024-06-06 01:16:31 -0400

    tweak new code

--- FastAnimations/Handlers/FadeHandler.cs
+++ FastAnimations/Handlers/FadeHandler.cs
@@ -1,17 +1,20 @@
-﻿using Pathoschild.Stardew.FastAnimations.Framework;
+using Microsoft.Xna.Framework;
+using Pathoschild.Stardew.FastAnimations.Framework;
 using StardewValley;
 using StardewValley.BellsAndWhistles;
 
 namespace Pathoschild.Stardew.FastAnimations.Handlers
 {
-    /// <summary>Handles the fade-to-black animation.</summary>
+    /// <summary>Handles the screen fade-to-black and fade-in animations.</summary>
     /// <remarks>See game logic in <see cref="Game1._update"/>.</remarks>
     internal class FadeHandler : BaseAnimationHandler
     {
+        /*********
+        ** Public methods
+        *********/
         /// <inheritdoc />
-        public FadeHandler(float multiplier) : base(multiplier)
-        {
-        }
+        public FadeHandler(float multiplier)
+            : base(multiplier) { }
 
         /// <inheritdoc />
         public override bool IsEnabled(int playerAnimationID)
@@ -28,14 +31,16 @@ namespace Pathoschild.Stardew.FastAnimations.Handlers
             );
         }
 
+
         /*********
-         ** Private methods
-         *********/
+        ** Private methods
+        *********/
         /// <summary>Handler the screen fade to black.</summary>
         /// <remarks>Derived from <see cref="ScreenFade.UpdateFadeAlpha"/>.</remarks>
         private void UpdateFadeToBlack()
         {
-            var time = Game1.currentGameTime;
+            GameTime time = Game1.currentGameTime;
+
             if (Game1.fadeIn)
                 Game1.fadeToBlackAlpha += (Game1.eventUp || Game1.farmEvent != null ? 0.0008f : 0.0019f) * time.ElapsedGameTime.Milliseconds;
             else if (!Game1.messagePause && !Game1.dialogueUp)
