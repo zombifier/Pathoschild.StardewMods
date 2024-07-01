60d55bc4f5246063168df264420d37a7bd6ea5e4
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2024-07-01 18:39:27 -0400

    simplify new check

--- LookupAnything/DataParser.cs
+++ LookupAnything/DataParser.cs
@@ -407,10 +407,7 @@ namespace Pathoschild.Stardew.LookupAnything
             {
                 string qualifiedMachineId = entryKey; // avoid referencing loop variable in closure
 
-                if (ItemRegistry.ResolveMetadata(qualifiedMachineId) == null)
-                    continue;
-
-                if (machineData?.OutputRules?.Count is not > 0)
+                if (!ItemRegistry.Exists(qualifiedMachineId) || machineData?.OutputRules?.Count is not > 0)
                     continue;
 
                 RecipeIngredientModel[] additionalConsumedItems =
